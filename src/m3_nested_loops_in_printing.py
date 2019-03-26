32f05f453bec2e8f84c6b29e206112489030f2ce
brackinPatsy <brackin@rose-hulman.edu>
2019-03-26 08:38:35 -0400

    Now on session 18 and so far things are working!

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -202,7 +202,7 @@ def alternating_brackets(m, n):
     Precondition:  m and n are positive integers with m >= n.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Tdone: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -210,7 +210,14 @@ def alternating_brackets(m, n):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(m, n-1, -1):
+        for j in range(k):
+            if j % 2 == 0:
+                value = '['
+            else:
+                value = ']'
+            print(value, end='')
+        print()
 
 def run_test_triangle_same_number_in_each_row():
     """ Tests the    triangle_same_number_in_each_row    function. """
@@ -288,7 +295,7 @@ def triangle_all_numbers_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # done: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -296,7 +303,10 @@ def triangle_all_numbers_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for i in range(1,r+1):
+        for j in range(i):
+            print(i, end ='')
+        print()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
